ccd631e7e208b7cc0b1f6b81de17f195f8bfde24
ezhh <owlecho@live.com>
2018-04-24 19:58:46 +0100

    Apples!

---
+++ init.lua
@@ -0,0 +1,64 @@
+
+-- alternative apple
+minetest.register_node("endless_apples:apple", {
+	description = "Apple",
+	drawtype = "plantlike",
+	tiles = {"default_apple.png"},
+	inventory_image = "default_apple.png",
+	paramtype = "light",
+	sunlight_propagates = true,
+	walkable = false,
+	is_ground_content = false,
+	selection_box = {
+		type = "fixed",
+		fixed = {-3 / 16, -7 / 16, -3 / 16, 3 / 16, 4 / 16, 3 / 16}
+	},
+	groups = {fleshy = 3, dig_immediate = 3, flammable = 2,
+		leafdecay = 3, leafdecay_drop = 1, food_apple = 1},
+	on_use = minetest.item_eat(2),
+	sounds = default.node_sound_leaves_defaults(),
+})
+
+-- override default apple
+minetest.register_node(":default:apple", {
+	description = "Apple",
+	drawtype = "plantlike",
+	tiles = {"default_apple.png"},
+	inventory_image = "default_apple.png",
+	paramtype = "light",
+	sunlight_propagates = true,
+	walkable = false,
+	is_ground_content = false,
+	selection_box = {
+		type = "fixed",
+		fixed = {-3 / 16, -7 / 16, -3 / 16, 3 / 16, 4 / 16, 3 / 16}
+	},
+	groups = {fleshy = 3, dig_immediate = 3, flammable = 2,
+		leafdecay = 3, leafdecay_drop = 1, food_apple = 1,
+		not_in_creative_inventory = 1},
+	on_use = minetest.item_eat(2),
+	sounds = default.node_sound_leaves_defaults(),
+
+	after_dig_node = function(pos, oldnode, oldmetadata, digger)
+		minetest.set_node(pos, {name = "endless_apples:apple_mark", param2 = 1})
+		minetest.get_node_timer(pos):start(3)
+	end,
+	drop = "endless_apples:apple"
+})
+
+-- air node to mark apple pos
+minetest.register_node("endless_apples:apple_mark", {
+	description = "Air!",
+	drawtype = "airlike",
+	paramtype = "light",
+	sunlight_propagates = true,
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	drop = "",
+	groups = {not_in_creative_inventory = 1},
+	on_timer = function(pos, elapsed)
+		minetest.set_node(pos, {name = "default:apple"})
+	end
+})
