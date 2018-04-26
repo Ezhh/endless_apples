31833be62c3f822345a86953f1a9f08515f846e5
dopik <33575686+dopik@users.noreply.github.com>
2018-04-26 22:41:53 +0200

    Remove the fix for #3

--- init.lua
+++ init.lua
@@ -38,20 +38,7 @@ minetest.register_node(":default:apple", {
 		not_in_creative_inventory = 1},
 	on_use = minetest.item_eat(2),
 	sounds = default.node_sound_leaves_defaults(),
-
 	after_dig_node = function(pos, oldnode, oldmetadata, digger)
-		local nodes = minetest.find_nodes_in_area({x = pos.x -2, y = pos.y -2, z = pos.z -2}, {x = pos.x +2, y = pos.y +2, z = pos.z +2}, "group:leaves")
-		local c = 0
-		for k, nodepos in pairs(nodes) do
-			if minetest.get_node(nodepos).param2 == 0 then
-				c = c +1
-			end
-		end
-		
-		if c < 10 then -- only make apple regrowable if at least 10 non user-placed leaves are around
-			return
-		end
-		
 		minetest.set_node(pos, {name = "endless_apples:apple_mark", param2 = 1})
 		minetest.get_node_timer(pos):start(math.random(120, 300))
 	end,
@@ -71,8 +58,8 @@ minetest.register_node("endless_apples:apple_mark", {
 	drop = "",
 	groups = {not_in_creative_inventory = 1},
 	on_timer = function(pos, elapsed)
-		if minetest.get_node(pos).param1 < 12 then -- enough light to grow apples?
-			minetest.get_node_timer(pos):start(math.random(40, 75)) -- no: try again in 40-75 seconds
+		if minetest.get_node(pos).param1 < 12 then
+			minetest.get_node_timer(pos):start(math.random(40, 75))
 			return
 		end
 		minetest.set_node(pos, {name = "default:apple"})
