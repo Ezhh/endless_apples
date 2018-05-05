e420d68b25349f1fdf1654236a44553680eea969
ezhh <owlecho@live.com>
2018-05-05 01:47:07 +0100

    Remove need for second apple type

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 
--- alternative apple
-minetest.register_node("endless_apples:apple", {
+-- override default apple
+minetest.register_node(":default:apple", {
 	description = "Apple",
 	drawtype = "plantlike",
 	tiles = {"default_apple.png"},
@@ -17,35 +17,17 @@ minetest.register_node("endless_apples:apple", {
 		leafdecay = 3, leafdecay_drop = 1, food_apple = 1},
 	on_use = minetest.item_eat(2),
 	sounds = default.node_sound_leaves_defaults(),
-})
 
--- override default apple
-minetest.register_node(":default:apple", {
-	description = "Apple",
-	drawtype = "plantlike",
-	tiles = {"default_apple.png"},
-	inventory_image = "default_apple.png",
-	paramtype = "light",
-	sunlight_propagates = true,
-	walkable = false,
-	is_ground_content = false,
-	selection_box = {
-		type = "fixed",
-		fixed = {-3 / 16, -7 / 16, -3 / 16, 3 / 16, 4 / 16, 3 / 16}
-	},
-	groups = {fleshy = 3, dig_immediate = 3, flammable = 2,
-		leafdecay = 3, leafdecay_drop = 1, food_apple = 1,
-		not_in_creative_inventory = 1},
-	on_use = minetest.item_eat(2),
-	sounds = default.node_sound_leaves_defaults(),
+	after_place_node = function(pos, placer, itemstack)
+		minetest.set_node(pos, {name = "default:apple", param2 = 1})
+	end,
+
 	after_dig_node = function(pos, oldnode, oldmetadata, digger)
-		minetest.set_node(pos, {name = "endless_apples:apple_mark", param2 = 1})
-		minetest.get_node_timer(pos):start(math.random(120, 300))
+		if oldnode.param2 == 0 then
+			minetest.set_node(pos, {name = "endless_apples:apple_mark"})
+			minetest.get_node_timer(pos):start(math.random(120, 300))
+		end
 	end,
-	after_place_node = function(pos, placer, itemstack, pointed_thing)
-		minetest.set_node(pos, {name = "endless_apples:apple", param2 = 1})		
-	end,
-	drop = "endless_apples:apple"
 })
 
 -- air node to mark apple pos
