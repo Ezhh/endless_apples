5c8b953ab7bb4def22a72a28c670443c3874f4eb
ezhh <owlecho@live.com>
2018-05-15 18:06:07 +0100

    Switch to override_item

--- init.lua
+++ init.lua
@@ -1,27 +1,6 @@
 
 -- override default apple
-minetest.register_node(":default:apple", {
-	description = "Apple",
-	drawtype = "plantlike",
-	tiles = {"default_apple.png"},
-	inventory_image = "default_apple.png",
-	paramtype = "light",
-	sunlight_propagates = true,
-	walkable = false,
-	is_ground_content = false,
-	selection_box = {
-		type = "fixed",
-		fixed = {-3 / 16, -7 / 16, -3 / 16, 3 / 16, 4 / 16, 3 / 16}
-	},
-	groups = {fleshy = 3, dig_immediate = 3, flammable = 2,
-		leafdecay = 3, leafdecay_drop = 1, food_apple = 1},
-	on_use = minetest.item_eat(2),
-	sounds = default.node_sound_leaves_defaults(),
-
-	after_place_node = function(pos, placer, itemstack)
-		minetest.set_node(pos, {name = "default:apple", param2 = 1})
-	end,
-
+minetest.override_item("default:apple", {
 	after_dig_node = function(pos, oldnode, oldmetadata, digger)
 		if oldnode.param2 == 0 then
 			minetest.set_node(pos, {name = "endless_apples:apple_mark"})
